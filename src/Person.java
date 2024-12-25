f54190dc07dd99049877b0cc1246227f7eefed82
Hannah2989 <hana.rostami2020@gmail.com>
2024-12-25 22:41:59 +0330

    Student class commit.

--- src/Person.java
+++ src/Person.java
@@ -5,5 +5,22 @@ public abstract class Person {
     String email;
     int age;
 
+    public Person(long id, String name, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
     abstract void showDetails();
 }
---
+++ src/Student.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends Person{
+
+    List<String> enrolledCourses = new ArrayList<>();
+
+    public Student(long id, String name,String email, int age, List<String> enrolledCourses) {
+        super(id, name, email, age);
+        this.enrolledCourses = enrolledCourses;
+    }
+
+    @Override
+    void showDetails() {
+        System.out.println("Student{" +
+                "age=" + age +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                "enrolledCourses=" + enrolledCourses + '\''
+                +
+                '}');
+    }
+}
